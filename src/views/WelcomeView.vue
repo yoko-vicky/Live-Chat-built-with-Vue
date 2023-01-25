631d5dc1a57a9b624599e4a8a486cbe42df99d4e
yocosaka <sakayoco55@gmail.com>
2023-01-25 13:49:46 +0900

    Add WelcomeView

---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container welcome"><p>welcome</p></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.welcome {
+  text-align: center;
+  padding: 20px 0;
+}
+/* form styles */
+.welcome form {
+  width: 300px;
+  margin: 20px auto;
+}
+.welcome label {
+  display: block;
+  margin: 20px 0 10px;
+}
+.welcome input {
+  width: 100%;
+  padding: 10px;
+  border-radius: 20px;
+  border: 1px solid #eee;
+  outline: none;
+  color: #999;
+  margin: 10px auto;
+}
+.welcome span {
+  font-weight: bold;
+  text-decoration: underline;
+  cursor: pointer;
+}
+.welcome button {
+  margin: 20px auto;
+}
+</style>
