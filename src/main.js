32c8f864d7cf9106095e89b2957ed903dde0b454
yocosaka <sakayoco55@gmail.com>
2023-01-25 17:30:31 +0900

    Add condition to create app only when app does not exist

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import './assets/main.css';
+import { projectAuth } from './firebase/config';
 
-createApp(App).use(router).mount('#app');
+let app;
+
+projectAuth.onAuthStateChanged(() => {
+  if (!app) {
+    app = createApp(App).use(router).mount('#app');
+  }
+});
