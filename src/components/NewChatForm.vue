a662b5d8cd433c038271b1bfcf2f61f100bca96a
yocosaka <sakayoco55@gmail.com>
2023-01-25 17:53:04 +0900

    Add NewChatForm

---
+++ src/components/NewChatForm.vue
@@ -0,0 +1,60 @@
+<template>
+  <form>
+    <textarea
+      placeholder="Type a message and hit Enter to send..."
+      v-model="message"
+      @keypress.enter.prevent="handleSubmit"
+    ></textarea>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue';
+import getUser from '@/composables/getUser';
+import { timestamp } from '@/firebase/config';
+
+export default {
+  setup() {
+    const { user } = getUser();
+    const message = ref('');
+
+    const handleSubmit = async () => {
+      const chat = {
+        user: user.value.displayName,
+        message: message.value,
+        createdAt: timestamp(),
+      };
+      console.log(chat);
+      message.value = '';
+    };
+
+    return {
+      message,
+      handleSubmit,
+    };
+  },
+};
+</script>
+
+<style scoped>
+form {
+  margin: 10px;
+}
+textarea {
+  width: 100%;
+  max-width: 100%;
+  margin-bottom: 6px;
+  padding: 10px;
+  box-sizing: border-box;
+  border: 0;
+  border-radius: 20px;
+  font-family: inherit;
+  outline: none;
+}
+.error {
+  text-align: center;
+  color: #ff2a58;
+  font-size: 12px;
+  padding: 10px 0;
+}
+</style>
--- src/views/ChatroomView.vue
+++ src/views/ChatroomView.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="container">
     <NavBar />
+    <NewChatForm />
   </div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue';
+import NewChatForm from '@/components/NewChatForm.vue';
 import getUser from '@/composables/getUser';
 import { useRouter } from 'vue-router';
 import { watch } from 'vue';
 
 export default {
-  components: { NavBar },
+  components: { NavBar, NewChatForm },
   setup() {
     const { user } = getUser();
     const router = useRouter();
